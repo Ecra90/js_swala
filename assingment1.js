567f878a5c4608a4b8515f0181bb7c761027a588
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-25 13:47:33 +0300

    js_swala

---
+++ assingment1.js
@@ -0,0 +1,10 @@
+// creating three string variablles to store username,password and email and log them in the console
+let username ='Ecrasam';
+let password = 3425;
+let email = 'Ecrasam2@gmail.com';
+console.log (username);
+console.log(password);
+console.log(email);
+// creating an array of 5 counties and log the second county in the console using it's index
+let counties = ['kenya','nigeria','lagos','algeria','kiambu'];
+console.log(counties[2]);
