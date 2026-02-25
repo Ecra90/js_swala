a73ad5f3a632c60c70071a57ef9be36688bd7dca
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-25 13:39:42 +0300

    js_swala

--- lesson1e.js
+++ lesson1e.js
@@ -1,4 +1,6 @@
 // objects and array
+// An array is a special type of object uesd to store a list of values in an order
+// manner.Arrays are indexed by numbers starting from 0.
 let person = {
     name :"Sam",
     age :20,
@@ -8,5 +10,5 @@ let person = {
 console.log(person)
 // array of fruits and console.log
 let fruits =["oranges","mangoes","apples"]
-console.log(fruits)
-console.log(fruits)
+console.log(fruits);
+console.log(fruits[3])
