e06d6089bcc099146a5c9fde7456d421bc1a60e4
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-25 13:33:34 +0300

    js_swala

---
+++ lesson1d.js
@@ -0,0 +1,23 @@
+// js string -- characters are enclosed in single or double quotes
+let username = "Sam";
+console.log(username);
+//  js numbers = integers,float
+let age = 43;
+console .log(age)
+let pi = 4.2
+// floating number
+console.log(pi);
+
+// booleabn
+// =true and false
+let isAdult =false;
+let isRgistered =true;
+console.log(isAdult);
+console.log(isRgistered);
+// log
+// undefined
+let somevariables;
+console.log(somevariables)
+// null
+let user = null;
+console.log(user)
---
+++ lesson1e.js
@@ -0,0 +1,11 @@
+// objects and array
+let person = {
+    name :"Sam",
+    age :20,
+    isadmin :true
+};
+// log
+console.log(person)
+// array of fruits and console.log
+let fruits =["oranges","mangoes","apples"]
+console.log(fruits)
