bbfdc188809fd606ed2d76c61b931024e88d0b0a
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-26 10:23:00 +0300

    js_swala

---
+++ assignment1a.js
@@ -0,0 +1,15 @@
+let distance =300
+if 
+    (distance<=0)
+    {console.log("invalid")}
+else if
+    (distance <=100)
+    {console.log("pay 5 USD")}
+else if
+    (distance<=500)
+    {console.log("pay 10 USD")}
+else if
+    (distance<=1000)    
+    {console("pay 20 USD")}
+else
+    {console.log("pay 40 USD")}
--- lesson2c.js
+++ lesson2c.js
@@ -1,8 +1,11 @@
 // if ....elseif......else statement
-let age =16;
-if (age <18)
-{console.log("you are a minor")}
+let age =18;
+if (age <18){
+    console.log("you are a minor")
+}
 else if (age>=18 && age<=20)
-{console.log("adult and but not old enough to drink")}
-else
-    {console.log("can drink alcohol")}
+{console.log("adult and but not old enough to drink")
+}
+else {
+    console.log("can drink alcohol")
+}
