7e43e65d5084d524567f530ebe1b49206277c078
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-26 09:51:03 +0300

    js_swala

--- lesson2c.js
+++ lesson2c.js
@@ -1,1 +1,20 @@
 // if ....elseif......else statement
+let age =18;
+if (age >18){
+    console.log("you are an adult")
+}
+else if 
+(age <18)
+{
+    console.log("you are a minor")
+}
+else if 
+(age<=20)
+{
+    console.log("Not old enough to drink")
+}
+else if
+(age >=21)
+{
+    console.log("can drink alcohol")
+}
