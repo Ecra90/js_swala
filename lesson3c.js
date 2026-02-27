1633dfc07c82556ae9b5269c7de572e2f2be060e
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-27 16:42:45 +0300

    js_swala

--- lesson3c.js
+++ lesson3c.js
@@ -4,3 +4,11 @@ function sayHello(){
     console.log("hello world")
 }
 sayHello()
+// to add two numbers
+function add() {
+    let num1=34
+    let num2=45
+    let answer= num1+num2
+    console.log(`the answer is;${answer}`)
+}
+add()
