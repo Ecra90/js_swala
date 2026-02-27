56e24c97fa3c09793b90f5eb8b8c3978ea1b5cbe
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-27 15:03:13 +0300

    js_swala

---
+++ assignment1b.js
@@ -0,0 +1,34 @@
+let grossincome =5999;
+if (grossincome<5999){
+    console.log("contribution=150")
+}
+else if
+    (grossincome>6000 && grossincome<=7999){
+        console.log("contribution=300")
+    }
+else if
+    (grossincome>8000 && grossincome<=11999){
+        console.log("contribution=400")
+    }
+else if
+    (grossincome>12000 && grossincome<=14999){
+        console.log("contribution=500")
+    }
+else if
+    (grossincome>20000 && grossincome<=24999){
+        console.log("contribution=750")
+    }
+else if
+    (grossincome>25000 && grossincome<=29999){
+        console.log("contribution=850")
+    }
+else if
+    (grossincome>30000 && grossincome<=49999){
+        console.log("contribution=1000")
+    }
+else if
+    (grossincome>50000 && grossincome<=99999){
+        console.log("contribution=1500")
+    }
+else
+    console.log("contribution=2000")
