fe48d5d82591ef889abc779207e52a73d77a4f50
Ecra90 <sharleymuthoni2@gmail.com>
2026-02-27 16:36:42 +0300

    js_swala

---
+++ lesson3c.js
@@ -0,0 +1,6 @@
+// fuction==block ofreusable code performing a specific test
+// has got three parts,function definition,function body and calling the function
+function sayHello(){
+    console.log("hello world")
+}
+sayHello()
